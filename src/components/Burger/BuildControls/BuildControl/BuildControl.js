44b8ac829aa568489d7f4de57837158570531474
Retr0_0x315 <barryblando@gmail.com>
2018-08-03 21:15:49 +0800

    Refactor Code.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,16 +3,16 @@ import React from 'react';
 
 import classes from './BuildControl.css';
 
-const buildControl = props => (
+const buildControl = ({ added, removed, disabled, label }) => (
   <div className={classes.BuildControl}>
-    <div className={classes.Label}>{props.label}</div>
+    <div className={classes.Label}>{label}</div>
     <button
       className={classes.Less}
-      onClick={props.removed}
-      disabled={props.disabled}>Less</button>
+      onClick={removed}
+      disabled={disabled}>Less</button>
     <button
       className={classes.More}
-      onClick={props.added}>More</button>
+      onClick={added}>More</button>
   </div>
 );
 
