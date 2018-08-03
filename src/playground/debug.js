204a9621d430801c02d679ed60a84bfa3dbc8bc8
Retr0_0x315 <barryblando@gmail.com>
2018-08-03 21:32:12 +0800

    added playground.

--- src/playground/debug.js
+++ src/playground/debug.js
@@ -1,6 +1,6 @@
 const disabledInfo = {
-  salad: 2,
-  bacon: 3,
+  salad: 0,
+  bacon: 0,
   cheese: 4,
   meat: 2,
 };
@@ -12,6 +12,22 @@ console.log(Object.keys(disabledInfo).map(igKey => {
 for (let key in disabledInfo) {
   // check if true then disable the specific ingredient button, i.e { salad: true, meat: false, etc. }
   disabledInfo[key] = disabledInfo[key] <= 0;
+  key // ?
+  disabledInfo // ?
   console.log(disabledInfo[key]);
 }
+
+const enoughDragonSpawned = Math.random() > 0.75; // ?
+
+!enoughDragonSpawned // ?
+
+if (!enoughDragonSpawned) {
+  console.log('+1')
+} else  {
+  console.log('done');
+}
+
+
+
+
 
