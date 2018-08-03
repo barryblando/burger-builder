784dec7b64d3510d2a2a701042dd92f4d6138f86
Retr0_0x315 <barryblando@gmail.com>
2018-08-03 21:17:52 +0800

    Refactor Code & Added Checkout Cancel & Continue button.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients)
+  const { ingredients, price, purchaseCanceled, purchaseContinued } = props;
+
+  const ingredientSummary = Object.keys(ingredients)
     .map(igKey => {
       return (
         <li key={igKey}>
-          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {ingredients[igKey]}
         </li>
       );
     });
 
   return (
-    <Aux>
+    <Fragment>
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients: </p>
       <u>
         {ingredientSummary}
       </u>
-      <p>Continue to Checkout?(Temporary)</p>
-    </Aux>
+      <p><strong>Total Price: {price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button
+        btnType="Danger"
+        clicked={purchaseCanceled}>CANCEL</Button>
+      <Button
+        btnType="Success"
+        clicked={purchaseContinued}>CONTINUE</Button>
+    </Fragment>
   );
 };
 
