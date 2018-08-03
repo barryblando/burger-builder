bbe9a7cab6842ece5fb108c94d544c2447d0e8e2
Retr0_0x315 <barryblando@gmail.com>
2018-08-03 21:16:05 +0800

    Refactor Code.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,24 +11,24 @@ const controls = [
   { label: 'Meat', type: 'meat' },
 ];
 
-const buildControls = props => (
+const buildControls = ({ price, ingredientAdded, ingredientRemoved, disabled, purchaseable, ordered }) => (
   <div className={classes.BuildControls}>
-    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+    <p>Current Price: <strong>{price.toFixed(2)}</strong></p>
     {/* passing ctrl.type in BuildControl as prop causes unnecessary loop, to prevent that
       from happening just omit it & put listener on added prop to just return everything back up */}
     {controls.map(ctrl => (
         <BuildControl
           key={ctrl.label}
           label={ctrl.label}
-          added={() => props.ingredientAdded(ctrl.type)}
-          removed={() => props.ingredientRemoved(ctrl.type)}
-          disabled={props.disabled[ctrl.type]}
+          added={() => ingredientAdded(ctrl.type)}
+          removed={() => ingredientRemoved(ctrl.type)}
+          disabled={disabled[ctrl.type]}
         />
     ))}
     <button
       className={classes.OrderButton}
-      disabled={!props.purchaseable}
-      onClick={props.ordered}>ORDER NOW</button>
+      disabled={!purchaseable}
+      onClick={ordered}>ORDER NOW</button>
   </div>
 );
 
