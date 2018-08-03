89f29a16998230e875367cfa5ac556cc4491f112
Retr0_0x315 <barryblando@gmail.com>
2018-08-03 21:24:24 +0800

    Added Component update lifecycle hook.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-      >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+  // Prevent OrderSummary from updating when adding and removing
+  // react only when show is true, don't react on clicked listener
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    const { show, modalClosed, children } = this.props;
+
+    return (
+      <Fragment>
+        <Backdrop show={show} clicked={modalClosed}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: show ? '1' : '0'
+          }}
+          >
+          {children}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Modal;
