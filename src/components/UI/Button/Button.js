84f0e7edf0b7db91731d03a2d6bb2dc9dfb4ae53
Retr0_0x315 <barryblando@gmail.com>
2018-08-22 15:59:56 +0800

    Refactor Button to add disabled property & added disabled css style.

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,10 +3,11 @@ import React from 'react'
 import classes from './Button.css';
 
 const button = props => {
-  const { btnType, clicked, children } = props;
+  const { btnType, clicked, children, disabled } = props;
 
   return (
     <button
+      disabled={disabled}
       className={[classes.Button, classes[btnType]].join(' ')}
       onClick={clicked}>{children}</button>
   );
