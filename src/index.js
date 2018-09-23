404eea55e245f144549c458b55cda8517dd1f0d2
Retr0_0x315 <barryblando@gmail.com>
2018-09-23 21:54:11 +0800

    refactored store to use in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './index.css';
 
@@ -8,10 +9,14 @@ import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
+import configureStore from './store/configureStore';
+
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={configureStore}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
