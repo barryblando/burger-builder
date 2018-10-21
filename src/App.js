8b5979ea6dfd67f50ff73771e04d1f12f7a8c7ad
Retr0_0x315 <barryblando@gmail.com>
2018-10-21 22:04:01 +0800

    protected the routes.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { Component, Fragment } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+// Containers Component
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
+
+import * as actions from './store/actions/index';
 
 class App extends Component {
+
+  componentDidMount() {
+    // every time App mounted check auth state
+    this.props.onTryAutoSignUp();
+  }
+
   render() {
+    const { isAuthenticated } = this.props;
+
+    let routes = (
+      <Switch>
+        <Route path="/" component={BurgerBuilder} exact />
+        <Route path="/auth" component={Auth} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/" component={BurgerBuilder} exact />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
     return (
       <Fragment>
         <Layout>
-          <Switch>
-            {/* <Route path="/" render={() => <h1>HOME</h1>} exact /> */}
-            {/* <Route path="/burger-builder" component={BurgerBuilder} /> */}
-            <Route path="/" component={BurgerBuilder} exact />
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-          </Switch>
-          {/* <BurgerBuilder builder="Test: Receive Props"/> */}
+          {/* <Route path="/" render={() => <h1>HOME</h1>} exact /> */}
+          {routes}
         </Layout>
       </Fragment>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: !!state.auth.token
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignUp: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+
