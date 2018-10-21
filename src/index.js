755a722913b89899e4080118d20d256d902c1c28
Retr0_0x315 <barryblando@gmail.com>
2018-10-21 22:04:56 +0800

    update to CRA2.0+.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 import App from './App';
 
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 import configureStore from './store/configureStore';
 
@@ -20,4 +20,7 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById('root'));
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
