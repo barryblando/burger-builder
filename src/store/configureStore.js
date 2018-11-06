dcc8ed465626031276f4670fc9bec31a009e0046
Retr0_0x315 <barryblando@gmail.com>
2018-11-06 22:17:44 +0800

    added saga middleware & configure Watcher to listen to actions.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,24 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+// import Sagas
+import { watchAuth } from './sagas/';
 
 import rootReducer from './reducers/index';
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const loggerMiddleware = createLogger(); // to log prevState & nextState
+const sagaMiddleware = createSagaMiddleware();
 
 const configureStore = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(loggerMiddleware, thunkMiddleware))
+    composeEnhancers(applyMiddleware(loggerMiddleware, thunkMiddleware, sagaMiddleware))
   );
+
+// run watcher and listen
+sagaMiddleware.run(watchAuth);
 
 export default configureStore;
