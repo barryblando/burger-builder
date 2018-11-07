b5a39c4ef54739c0f7277486af6159c62da2939e
Retr0_0x315 <barryblando@gmail.com>
2018-11-07 11:26:19 +0800

    added sagas listener.

---
+++ src/store/sagas/index.js
@@ -0,0 +1,15 @@
+import { takeEvery } from 'redux-saga/effects';
+
+import * as actionTypes from '../actions/actionTypes';
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
+
+// Set Listeners
+export function* watchAuth() {
+  // TODO listen to actions given
+  // takeEvery will listen to certain actions and do something when they occur
+  // yields of takeEvery executes right after each other
+  yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
+  yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+  yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+  yield takeEvery(actionTypes.AUTH_CHECK_INITIAL_STATE, authCheckStateSaga);
+}
