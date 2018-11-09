3682417ced0d4fa5e0576d9ecedec35719e660f4
Retr0_0x315 <barryblando@gmail.com>
2018-11-09 22:27:29 +0800

    added burgerBuilder, watcherOrder, and the rootSaga to watch.

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,14 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, all, fork } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
-import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
+import {
+  logoutSaga,
+  checkAuthTimeoutSaga,
+  authUserSaga,
+  authCheckStateSaga
+} from './auth';
+import { initIngredientsSaga  } from './burgerBuilder';
+import { purchaseBurgerSaga, fetchOrdersSaga } from './order';
 
 // Set Listeners
 export function* watchAuth() {
@@ -12,4 +19,21 @@ export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
   yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
   yield takeEvery(actionTypes.AUTH_CHECK_INITIAL_STATE, authCheckStateSaga);
+}
+
+export function* watchBurgerBuilder() {
+  yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
+}
+
+export function* rootSaga() {
+  yield all([
+    fork(watchAuth),
+    fork(watchBurgerBuilder),
+    fork(watchOrder)
+  ]);
 }
