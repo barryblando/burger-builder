f74e7ac3bc7772a7cbf77f85ebde63081583abd4
BarryBlando <barryblando@gmail.com>
2019-02-14 12:52:54 +0800

    Refactored.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,23 +5,16 @@ import classes from './Burger.module.scss';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const renderBurger = ingredients => {
-  // TODO - turn ingredients object into an array of property keys using Object.keys
+  // TODO - Turn ingredients object into an array of property keys using Object.keys
+  // TODO - Access & turn ingredient key quantity/value into an array of undefined spaces using spread-ed Array Method
+  // - and then map over on each of it (quantity length) using only the index and then return ingredient
+  // - e.g Salad: 2, Bacon: 2 -- [0, 1].map((_, i) => i) // ? [[0, 1], [0, 1], ...]
+  // - flatten these arrays using reduce to pull out values of these inner arrays
+  // TODO - Return flattened array of elements. React can render this kind of syntax
+  // - [<BI type="salad" />, <BI type="salad" />, <BI type="bacon" />, <BI type="bacon" />] : [0, 1, 0, 1]
   let transformedIngredients = Object.keys(ingredients)
-    .map(igKey =>
-      // TODO - Access & turn ingredient key quantity/value into an array of undefined spaces using spread-ed Array Method
-      // and then map over on each of it (quantity length) using only the index and then return ingredient
-      // e.g Salad: 2, Bacon: 2 -- [0, 1].map((_, i) => i) // ? [[0, 1], [0, 1], ...]
-      // flatten these arrays using reduce to pull out values of these inner arrays
-      /* eslint-disable-next-line */
-      [...Array(ingredients[igKey])].map((_, i) => <BurgerIngredient key={igKey + i} type={igKey} />)
-    )
-    .reduce(
-      (arr, el) =>
-        // TODO - return flattened array of elements, separated by comma. React can render this kind of syntax
-        // [<BI type="salad" />, <BI type="salad" />, <BI type="bacon" />, <BI type="bacon" />] : [0, 1, 0, 1]
-        [...arr, ...el],
-      []
-    );
+    .map(igKey => [...Array(ingredients[igKey])].map((_, i) => <BurgerIngredient key={igKey + i} type={igKey} />)) /* eslint-disable-line */
+    .reduce((arr, el) => [...arr, ...el], []);
 
   if (transformedIngredients.length === 0) {
     transformedIngredients = <p className={classes.Burger__message}>Please start adding ingredients!</p>;
