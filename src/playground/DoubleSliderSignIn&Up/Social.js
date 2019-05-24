8849075bf776c39a089eb71ea9c57494b68e142e
barryblando <barryblando@gmail.com>
2019-05-24 11:56:27 +0800

    Thinking in React: Double Slider SignIn & Up

---
+++ src/playground/DoubleSliderSignIn&Up/Social.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const social = () => (
+  <div className="social-container">
+    <a href="#" className="social">
+      <i className="fab fa-facebook-f" />
+    </a>
+    <a href="#" className="social">
+      <i className="fab fa-google-plus-g" />
+    </a>
+    <a href="#" className="social">
+      <i className="fab fa-linkedin-in" />
+    </a>
+  </div>
+);
+
+export default social;
