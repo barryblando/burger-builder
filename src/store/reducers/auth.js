10c62631850681fea06788bfa014feafcc4620cc
barryblando <barryblando@gmail.com>
2019-05-24 11:51:12 +0800

    Format Reducers.

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -3,47 +3,45 @@ import { updateObject } from '../../utils/index';
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-  token: null,
+  // token: null,
   userId: null,
   error: null,
   loading: false,
   authRedirectPath: '/',
 };
 
-const authStart = (state, action) => updateObject(state, { error: null, loading: true });
+const authStart = state => updateObject(state, { error: null, loading: true });
 
-const authSuccess = (state, action) => {
-  return updateObject(state, {
-    token: action.idToken,
-    userId: action.localId,
+const authSuccess = (state, action) =>
+  updateObject(state, {
+    userId: action.userId,
     error: null,
-    loading: false
-  })
-};
+    loading: false,
+  });
 
-const authFail = (state, action) => {
-  return updateObject(state, {
+const authFail = (state, action) =>
+  updateObject(state, {
     error: action.error,
-    loading: false
-  })
-};
+    loading: false,
+  });
 
-const authLogout = (state, action) => {
-  return updateObject(state, { token: null, userId: null });
-};
-
-const setAuthRedirectPath = (state, action) => {
-  return updateObject(state, { authRedirectPath: action.path });
-}
+const authLogout = state => updateObject(state, { userId: null });
+const setAuthRedirectPath = (state, action) => updateObject(state, { authRedirectPath: action.path });
 
 const reducer = (state = initialState, action) => {
-  switch(action.type) {
-    case actionTypes.AUTH_START: return authStart(state, action);
-    case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
-    case actionTypes.AUTH_FAIL: return authFail(state, action);
-    case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
-    case actionTypes.SET_AUTH_REDIRECT_PATH: return setAuthRedirectPath(state, action);
-    default: return state;
+  switch (action.type) {
+    case actionTypes.AUTH_START:
+      return authStart(state, action);
+    case actionTypes.AUTH_SUCCESS:
+      return authSuccess(state, action);
+    case actionTypes.AUTH_FAIL:
+      return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT:
+      return authLogout(state, action);
+    case actionTypes.SET_AUTH_REDIRECT_PATH:
+      return setAuthRedirectPath(state, action);
+    default:
+      return state;
   }
 };
 
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -5,13 +5,13 @@ import { updateObject } from '../../utils/index';
  * INFO: don't use null in order state for now.
  * PROBLEM: when Order container gets mounted and in render execution it uses a .map or any declarative built-in methods, it gets TypeError
  * WHY: componentDidMount gets invoked immediately after a component is mounted (inserted into the tree). Initialization of orders are too late.
- * FIX: just use empty data i.e [] / {} 'cause we don't have error state to handle error response.
+ * TEMP FIX: just use empty data i.e [] / {} 'cause we don't have error state to handle error response.
  */
 
 const initialState = {
   orders: [],
   loading: false,
-  purchased: false
+  purchased: false,
 };
 
 const purchaseInit = (state, action) => updateObject(state, { purchased: false });
@@ -21,31 +21,38 @@ const purchaseBurgerSuccess = (state, action) => {
   const updateOrder = updateObject(state, {
     loading: false,
     purchased: true,
-    orders: [...state.orders, newOrder]
+    orders: [...state.orders, newOrder],
   });
   return updateObject(state, updateOrder);
 };
 const purchaseBurgerFail = (state, action) => updateObject(state, { loading: false });
 
 const fetchOrdersStart = (state, action) => updateObject(state, { loading: true });
-const fetchOrdersSuccess = (state, action) => {
-  return updateObject(state, {
+const fetchOrdersSuccess = (state, action) =>
+  updateObject(state, {
     orders: action.orders,
-    loading: false
+    loading: false,
   });
-};
 const fetchOrdersFail = (state, action) => updateObject(state, { loading: false });
 
 const reducer = (state = initialState, action) => {
-  switch(action.type) {
-    case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
-    case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
-    case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
-    case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
-    case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action);
-    case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
-    case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action);
-    default: return state;
+  switch (action.type) {
+    case actionTypes.PURCHASE_INIT:
+      return purchaseInit(state, action);
+    case actionTypes.PURCHASE_BURGER_START:
+      return purchaseBurgerStart(state, action);
+    case actionTypes.PURCHASE_BURGER_SUCCESS:
+      return purchaseBurgerSuccess(state, action);
+    case actionTypes.PURCHASE_BURGER_FAIL:
+      return purchaseBurgerFail(state, action);
+    case actionTypes.FETCH_ORDERS_START:
+      return fetchOrdersStart(state, action);
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return fetchOrdersSuccess(state, action);
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return fetchOrdersFail(state, action);
+    default:
+      return state;
   }
 };
 
