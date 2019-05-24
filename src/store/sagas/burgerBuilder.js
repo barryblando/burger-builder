b8764af7faa53b59e23aa5f83cea43b7141ac24f
barryblando <barryblando@gmail.com>
2019-05-24 11:49:50 +0800

    Use Redux Saga Firebase API instead of HTTP.

--- src/store/sagas/burgerBuilder.js
+++ src/store/sagas/burgerBuilder.js
@@ -1,19 +1,19 @@
-import axios from '../../axios-order';
 import { put, call } from 'redux-saga/effects';
+import reduxSagaFirebase from '../../config/firebaseConfig';
 
 import * as actions from '../actions';
 
-export function* initIngredientsSaga(action) {
+export function* initIngredientsSaga() {
   // TODO - get all the ingredients from firebase
   try {
-    const { data } = yield call(axios.get, '/ingredients.json');
+    const data = yield call(reduxSagaFirebase.database.read, 'ingredients');
     // TODO - destructure response data
     const { salad, bacon, cheese, meat } = data;
     // TODO - set the ingredients position starting with salad manually
     const ingredients = { salad, bacon, cheese, meat };
     // TODO - dispatch ingredients
     yield put(actions.setIngredients(ingredients));
-  } catch(e) {
+  } catch (e) {
     // TODO - if can't retrieve endpoint then set error to true
     yield put(actions.fetchIngredientsFailed());
   }
